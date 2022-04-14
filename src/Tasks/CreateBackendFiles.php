15cf7e74b699ce87ec307aca7bd4f6ce3e97a3e5
Bulent Akgul <bulent.akgul@me.com>
2022-04-14 17:48:05 +0300

    backend integration is component ready

--- src/Tasks/CreateBackendFiles.php
+++ src/Tasks/CreateBackendFiles.php
@@ -12,38 +12,94 @@ class CreateBackendFiles
 {
     public static function _(array $request, array $queue)
     {
-        if (self::isNotCreatable($queue)) return;
+        if (self::isNotCreatable($request, $queue)) return;
 
         Artisan::call(self::createCommand($request));
     }
 
-    private static function isNotCreatable(array $queue)
+    private static function isNotCreatable(array $request, array $queue): bool
     {
-        return !Settings::resourceOptions('each_page_has_controller')
-            || !file_exists(Path::realBase(Path::glue(['vendor', 'bakgul', 'laravel-file-creator'])))
-            || empty(self::creatables($queue));
+        return self::isNotView($request['type'])
+            || self::hasNoFileCreator()
+            || (self::hasNoController($queue) && self::hasNoClassPair($request));
     }
 
-    private static function creatables(array $queue): array
+    private static function isNotView($type)
     {
-        return array_filter($queue, fn ($x) => $x['variation'] == 'page' && $x['type'] == 'view');
+        return Isolation::type($type) != 'view';
+    }
+
+    private static function hasNoFileCreator()
+    {
+        return !file_exists(Path::realBase(Path::glue(['vendor', 'bakgul', 'laravel-file-creator'])));
+    }
+
+    private static function hasNoController($queue)
+    {
+        return !Settings::resourceOptions('each_page_has_controller')
+            || empty(self::creatables($queue, 'page'));
+    }
+
+    private static function hasNoClassPair(array $request): bool
+    {
+        return !$request['class'] && self::getType($request) != 'blade';
+    }
+
+    private static function getType($request)
+    {
+        return Settings::apps("{$request['app']}.type") ?? Isolation::extra($request['type']);
+    }
+
+    private static function creatables(array $queue, string $variation): array
+    {
+        return array_filter($queue, fn ($x) => $x['type'] == 'view' && $x['variation'] == $variation);
     }
 
     private static function createCommand(array $request): string
     {
         return implode(' ', array_filter([
             'create:file',
-            $request['name'],
+            self::setName($request),
             self::setType($request),
             $request['package'],
             $request['app'],
         ]));
     }
 
+    private static function setName(array $request)
+    {
+        return Text::changeTail(
+            $request['name'],
+            self::setPrefixedName($request),
+            Settings::seperators('folder')
+        );
+    }
+
+    private static function setPrefixedName(array $request): string
+    {
+        return implode('-', array_filter([self::setPrefix($request['type']), Isolation::name($request['name'])]));
+    }
+
+    private static function setPrefix(string $type): string
+    {
+        return Settings::resourceOptions('use_prefix') ? Settings::prefixes(Isolation::variation($type)) : '';
+    }
+
     private static function setType(array $request)
     {
-        $variation = Settings::apps("{$request['app']}.type") ?? Isolation::extra($request['type']);
+        return (Isolation::variation($request['type']) == 'page' ? 'controller' : 'component')
+            . self::appendVariation($request);
+    }
 
-        return 'controller' . Text::append($variation && $variation != 'blade' ? 'api' : '', Settings::seperators('modifier'));
+    private static function appendVariation($request)
+    {
+        return Text::append(self::isApi($request) ? 'api' : '', Settings::seperators('modifier'));
+    }
+
+    private static function isApi($request)
+    {
+        return Isolation::variation($request['type']) == 'page'
+            && self::getType($request) != 'blade'
+            && Settings::apps("{$request['app']}.router") != 'inertia';
     }
 }
