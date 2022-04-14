8bf5b64ac80d887848062ebee9638244eef4383c
Bulent Akgul <bulent.akgul@me.com>
2022-04-14 17:35:27 +0300

    temp settings change

--- config/resource_options.php
+++ config/resource_options.php
@@ -7,7 +7,7 @@ return [
         'high' => ['section', 'page']
     ],
     'each_page_has_controller' => false,
-    'share_low_levels_betwen_apps' => false,
+    'share_low_levels_betwen_apps' => true,
     'use_prefix' => true,
     'section_parents' => ['page', 'class', 'store'],
     'css' => 'sass',
