110ce9ce44c9ad8cbf0e5b7ce14df7472ab7b198
Bulent Akgul <bulent.akgul@me.com>
2022-04-17 09:59:48 +0300

    unsued methods removed

--- src/Services/ResourceServices/JsResourceService.php
+++ src/Services/ResourceServices/JsResourceService.php
@@ -13,28 +13,4 @@ class JsResourceService extends ResourceService
     {
         CallClass::_((new JsRequestService)->handle($request), 'resource', __NAMESPACE__);
     }
-
-    private function service(array $attr)
-    {
-        $tail = 'JsResourceService';
-        $base = __NAMESPACE__ . "\\{$tail}s";
-
-        foreach ([$attr['app_type'], Arry::get($attr, 'framework')] as $type) {
-            if (!$type) continue;
-            
-            $class = "{$base}\\{$this->setType($type)}{$tail}";
-
-            if (class_exists("\\{$class}")) return new $class;
-        }
-
-        return null;
-    }
-
-    protected function setType(string $type)
-    {
-        return ucfirst(match($type) {
-            'blade' => 'vanilla',
-            default => $type
-        });
-    }
 }
