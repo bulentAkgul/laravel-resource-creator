7faf0db860bc8bec275235860cb02be10046061d
Bulent Akgul <bulent.akgul@me.com>
2022-04-17 09:59:12 +0300

    duplicated brackets bug fixed

--- src/Functions/RequestFunctions/UpdateSchema.php
+++ src/Functions/RequestFunctions/UpdateSchema.php
@@ -7,8 +7,18 @@ class UpdateSchema
     public static function _(string $schema, string|array $search, string|array $replace): string
     {
         return str_replace(
-            array_map(fn ($x) => "{{ {$x} }}", is_array($search) ? $search : [$search]),
-            array_map(fn ($x) => $x ? "{{ {$x} }}" : '', is_array($replace) ? $replace : [$replace]),
+            array_map(
+                fn ($x) => "{{ {$x} }}",
+                is_array($search) ? $search : [$search]
+            ),
+            array_map(
+                fn ($x) => str_replace(
+                    ['{{ {{', '}} }}'],
+                    ['{{', '}}'],
+                    $x ? "{{ $x }}" : ''
+                ),
+                is_array($replace) ? $replace : [$replace]
+            ),
             $schema
         );
     }
