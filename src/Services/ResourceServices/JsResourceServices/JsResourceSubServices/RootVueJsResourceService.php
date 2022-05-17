c216c52d7c160cd43c2f7889a33e2fd1e319450e
Bulent Akgul <bulent.akgul@me.com>
2022-05-17 17:58:35 +0300

    js types creating method calls added

--- src/Services/ResourceServices/JsResourceServices/JsResourceSubServices/RootVueJsResourceService.php
+++ src/Services/ResourceServices/JsResourceServices/JsResourceSubServices/RootVueJsResourceService.php
@@ -3,13 +3,22 @@
 namespace Bakgul\ResourceCreator\Services\ResourceServices\JsResourceServices\JsResourceSubServices;
 
 use Bakgul\FileContent\Functions\CreateFile;
-use Bakgul\ResourceCreator\Services\RequestServices\ViewRequestServices\ViewRequestSubServices\RootVueJsRequestService;
+use Bakgul\Kernel\Helpers\Settings;
+use Bakgul\ResourceCreator\Services\RequestServices\JsRequestServices\JsRequestSubServices\RootVueJsRequestService;
 use Bakgul\ResourceCreator\Services\ResourceServices\JsResourceServices\VueJsResourceService;
 
 class RootVueJsResourceService extends VueJsResourceService
 {
     public function create(array $request): void
     {
-        CreateFile::_((new RootVueJsRequestService)->handle($request));
+        CreateFile::_(RootVueJsRequestService::main($request));
+        
+        if (Settings::resources('vue.options.store')) {
+            CreateFile::_(RootVueJsRequestService::{Settings::resources('vue.options.store')}($request));
+        }
+        
+        if (!in_array($request['attr']['router'], Settings::prohibitives('route'))) {
+            CreateFile::_(RootVueJsRequestService::router($request));
+        }
     }
 }
