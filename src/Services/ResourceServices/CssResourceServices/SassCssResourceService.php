57ea9400e784515df021d66fa8cf0694f7e8b4dd
Bulent Akgul <bulent.akgul@me.com>
2022-05-21 10:10:31 +0300

    Client styles creators added

--- src/Services/ResourceServices/CssResourceServices/SassCssResourceService.php
+++ src/Services/ResourceServices/CssResourceServices/SassCssResourceService.php
@@ -6,10 +6,23 @@ use Bakgul\FileContent\Functions\CreateFile;
 use Bakgul\ResourceCreator\Services\RegistrationServices\CssRegistrationServices\SassCssRegistrationService;
 use Bakgul\ResourceCreator\Services\RequestServices\CssRequestServices\SassCssRequestService;
 use Bakgul\ResourceCreator\Services\ResourceServices\CssResourceService;
+use Bakgul\ResourceCreator\Services\ResourceServices\CssResourceServices\CssResourceSubServices\RootSassResourceService;
 
 class SassCssResourceService extends CssResourceService
 {
     public function create(array $request): void
+    {
+        $request['attr']['variation'] == 'root'
+            ? $this->createRoot($request)
+            : $this->createFile($request);
+    }
+
+    private function createRoot($request)
+    {
+        (new RootSassResourceService)->create($request);
+    }
+
+    private function createFile($request)
     {
         $request = (new SassCssRequestService)->handle($request);
 
